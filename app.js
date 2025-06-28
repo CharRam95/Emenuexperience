ce8c17dc8e6ba671ae5b636402bf8398051a9a22
CharRam95 <menuexperience95@gmail.com>
2025-06-28 00:43:09 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-const routes = require('./routes/routes');
+const routes = require('./routes');
 const app = express();
 
 // Configuración
